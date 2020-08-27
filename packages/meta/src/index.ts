541fff5360fa238526c871903973be93b0423eee
hmsk <k.hamasaki@gmail.com>
2020-08-27 02:09:25 -0700

    Make a build of meta package executable

--- packages/meta/src/index.ts
+++ packages/meta/src/index.ts
@@ -1,1 +1,5 @@
-console.log('hi')
+import { readFileSync } from 'fs'
+import { join } from 'path'
+
+const address = JSON.parse(readFileSync(join(__dirname, 'japanese-addresses.json'), 'utf-8'))
+console.log(address)
