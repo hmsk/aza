c33a41db349c375e518b786900361ae27371027d
hmsk <k.hamasaki@gmail.com>
2020-09-16 01:10:58 -0700

    Build aza-meta with Rollup

---
+++ packages/meta/rollup.config.js
@@ -0,0 +1,12 @@
+import json from '@rollup/plugin-json'
+import { nodeResolve } from '@rollup/plugin-node-resolve'
+import typescript from '@rollup/plugin-typescript'
+
+export default {
+  input: 'src/index.ts',
+  output: {
+    dir: 'dist',
+    format: 'cjs',
+  },
+  plugins: [typescript(), nodeResolve(), json()],
+}
