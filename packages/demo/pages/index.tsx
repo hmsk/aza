0406c11e2d9008960b702f907b08d25b11834c44
hmsk <k.hamasaki@gmail.com>
2020-10-14 23:56:13 -0700

    Avoid multiple loading

--- packages/demo/pages/index.tsx
+++ packages/demo/pages/index.tsx
@@ -4,12 +4,15 @@ import { AzaMetaWithName } from 'aza-meta'
 const Index: FunctionComponent = () => {
   const [result, setResult] = useState<AzaMetaWithName[]>([])
   const [term, setTerm] = useState('白金台')
+  const [loading, setLoading] = useState(false)
 
   useEffect(() => {
+    setLoading(true)
     fetch(`/api/search?term=${term}`)
       .then(res => res.json())
       .then(({ result }: { result: AzaMetaWithName[] }) => {
         setResult(result)
+        setLoading(false)
       })
   }, [term])
 
@@ -17,7 +20,7 @@ const Index: FunctionComponent = () => {
     <div>
       <h1>aza demo</h1>
       <input type="text" value={term} onChange={e => setTerm(e.currentTarget.value)} />
-      <p>Total: {result.length}</p>
+      <p>Total: { loading ? "Loading..." : result.length}</p>
       <ul>
         { result.map(res => {
           return <li key={res.id}>{res.name}（{res.prefecture} {res.municipality}）</li>
