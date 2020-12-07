f53251ec4f5c15e1484ec3cfd6b8a2bec83e0441
hmsk <k.hamasaki@gmail.com>
2020-12-07 00:40:08 -0800

    [demo] fix type def for *.md to resolve errors on build

--- packages/demo/next-env.d.ts
+++ packages/demo/next-env.d.ts
@@ -1,9 +1,9 @@
 /// <reference types="next" />
 /// <reference types="next/types/global" />
 
-import { FunctionComponent } from "react"
+declare module '*.md' {
+  import { FunctionComponent } from 'react'
 
-declare module "*.md" {
   export const attributes: Record<string, unknown>
   export const react: FunctionComponent
   export const html: string
