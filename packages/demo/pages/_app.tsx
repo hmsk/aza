45575d7dd5509e60b003f24158d0f428662aff43
hmsk <k.hamasaki@gmail.com>
2020-12-10 22:52:34 -0800

    [demo] have a global css to write some utilities

--- packages/demo/pages/_app.tsx
+++ packages/demo/pages/_app.tsx
@@ -1,5 +1,5 @@
 import { AppProps } from 'next/dist/next-server/lib/router/router'
-import 'tailwindcss/tailwind.css'
+import '../styles/global.css'
 
 function AzaDemo({ Component, pageProps }: AppProps) {
   return <Component {...pageProps} />
