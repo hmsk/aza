210f1231a1c2b7de51740cd31e32309df86e3b87
hmsk <k.hamasaki@gmail.com>
2020-12-13 15:32:16 -0800

    [demo] give fundamental meta tags

--- packages/demo/pages/_app.tsx
+++ packages/demo/pages/_app.tsx
@@ -1,8 +1,16 @@
+import Head from 'next/head'
 import { AppProps } from 'next/dist/next-server/lib/router/router'
 import '../styles/global.css'
 
 function AzaDemo({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return <>
+    <Head>
+      <title>Aza: 日本の住所情報入力のインタフェースについて考える</title>
+      <meta property="og:title" content="Aza: 日本の住所情報入力のインタフェースについて考える" />
+      <meta name="author" content="@hmsk" />
+    </Head>
+    <Component {...pageProps} />
+  </>
 }
 
 export default AzaDemo
